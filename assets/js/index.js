971f36515f1031fc91a85257ff7e9f375893ef8e
Matteo <matteonini95@gmail.com>
2020-11-30 20:46:28 +0100

    [ADD] Function for show center menu and provide basic user navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-//On load check if mobile
+//On load check device
 window.onload = function () {
   let ckMobile = {
     Android: function () {
@@ -28,13 +28,15 @@ window.onload = function () {
   };
 
   if (ckMobile.any()) {
+    //If is mobile
     //Remove Desktop elements
     let allDesktop = document.querySelectorAll(".desktop");
     allDesktop.forEach((desktop) => {
       desktop.style.display = "none";
     });
-    getStarted();
+    getStarted(); //first function that handle the clickable Get Started Button
   } else {
+    //if is desktop
     //Remove all mobile elements
     let allMobile = document.querySelectorAll("section");
     allMobile.forEach((mobile) => {
@@ -49,11 +51,12 @@ const home = document.querySelector(".homepage");
 const login = document.querySelector(".login");
 const userDash = document.querySelector(".dashboard");
 
+//Function for handle the eventListeners
 function getStarted() {
   //Get Started Button
   let startBtn = document.querySelector(".homepage__btn");
   startBtn.addEventListener("click", (event) => {
-    changePageAnimation(home, login, "next");
+    changePageAnimation(home, login, "next"); //Show login page
   });
 
   //GoBack Button in login section
@@ -63,10 +66,80 @@ function getStarted() {
   });
 }
 
-//page transition animation
+//login form validation
+function loginValidate(event) {
+  let form = document.querySelector(".login__form"); //login form
+  let canLog = true; //Flag for check the log status False: not logged | True: logged CHANGE IT FOR TEST
+  let name = document.querySelector("#name"); //input name
+  let password = document.querySelector("#password"); //input password
+  let Error = document.querySelectorAll(".login__form__validate"); //span
+  event.preventDefault();
+  //Add some basic check for the inputs
+  if (name.value == "" || name.value == null) {
+    name.focus();
+    name.style.border = "2px solid #f01f35";
+    name.style.color = "#f01f35";
+    Error[0].style.color = "#f01f35";
+    Error[0].innerHTML = "Please, enter a valid name";
+  } else if (password.value == "") {
+    password.focus();
+    password.style.border = "2px solid #f01f35";
+    password.style.color = "#f01f35";
+    Error[1].style.color = "#f01f35";
+    Error[1].innerHTML = "Password cannot be empty";
+  } else {
+    name.style.border = "1px solid #00B1BD";
+    name.style.color = "#00B1BD";
+    Error[0].style.color = "#00B1BD";
+    Error[0].innerHTML = "Ok!";
+    password.style.border = "1px solid #00B1BD";
+    password.style.color = "#00B1BD";
+    Error[1].style.color = "#00B1BD";
+    Error[1].innerHTML = "Ok!";
+    canLog = true;
+  }
+  //If all is ok, on submit,change page, than go to logged() and take the name value for showing later
+  if (canLog == true) {
+    changePageAnimation(login, userDash, "next");
+    form.addEventListener("submit", logged(name.value));
+  }
+}
+
+//user logged in
+function logged(username) {
+  let menu = document.querySelector(".dashboard__menu__ico"); //menu icon
+  let avatar = document.querySelector(".dashboard__menu__user"); //user icon
+  let sideMenu = document.querySelector(".menu__center"); //Center menu hidden
+  let menuElements = document.querySelectorAll(".menu__center li"); //Center menu elements
+
+  menu.addEventListener("click", (event) => {
+    if (sideMenu.style.display == "flex") sideMenu.style.display = "none";
+    else {
+      sideMenu.style.display = "flex"; //show menu
+      menuElements[0].innerHTML = "<a href='#'>Categories</a>"; //add link to Categories section
+      menuElements[1].innerHTML = "<a href='#'>Top Doctors</a>"; //add link to Top Doctors section
+      menuElements[2].style.display = "none"; //don't show it
+    }
+  });
+
+  avatar.addEventListener("click", (event) => {
+    if (sideMenu.style.display == "none") {
+      sideMenu.style.display = "flex";
+      menuElements[0].innerHTML = `Hi ${username}!`; //Take the username to show it
+      menuElements[1].innerHTML = "<a href='#'>Options</a>"; //Fake option page link
+      menuElements[2].innerHTML =
+        "<a href='#' onclick='changePageAnimation(userDash, login, \"prew\");'>Exit</a>"; //Exit link send user to login page
+    } else {
+      sideMenu.style.display = "none";
+    }
+  });
+}
+
+//Function for page transition animation
 function changePageAnimation(currentPage, finalPage, where) {
   let preloader = document.querySelector("#preloader");
 
+  //Define Keyframes
   let slideOutKeyFrames = [
     { transform: "translateX(0%)" },
     { transform: "translateX(-120%)" },
@@ -86,8 +159,10 @@ function changePageAnimation(currentPage, finalPage, where) {
     { transform: "translateX(-120%)" },
     { transform: "translateX(0)" },
   ];
+  //Let show preloader page and blur the current page
   preloader.style.opacity = "1";
   currentPage.style.filter = "blur(5px)";
+  //After one second, let change the page
   setTimeout(function () {
     if (where == "next") {
       let slideOut = currentPage.animate(slideOutKeyFrames, 700);
@@ -107,47 +182,4 @@ function changePageAnimation(currentPage, finalPage, where) {
   }, 1000);
 }
 
-//form validation
-function loginValidate(event) {
-  let form = document.querySelector(".login__form");
-  let log = true; //CHANGE IT FOR TEST
-  let name = document.querySelector("#name");
-  let password = document.querySelector("#password");
-  let Error = document.querySelectorAll(".login__form__validate");
-  event.preventDefault();
-  if (name.value == "" || name.value == null) {
-    name.focus();
-    name.style.border = "2px solid #f01f35";
-    name.style.color = "#f01f35";
-    Error[0].style.color = "#f01f35";
-    Error[0].innerHTML = "Please, enter a valid name";
-  } else {
-    name.style.border = "1px solid #00B1BD";
-    name.style.color = "#00B1BD";
-    Error[0].style.color = "#00B1BD";
-    Error[0].innerHTML = "Ok!";
-  }
-  if (password.value == "") {
-    password.focus();
-    password.style.border = "2px solid #f01f35";
-    password.style.color = "#f01f35";
-    Error[1].style.color = "#f01f35";
-    Error[1].innerHTML = "Password cannot be empty";
-  } else {
-    name.style.border = "1px solid #00B1BD";
-    name.style.color = "#00B1BD";
-    Error[0].style.color = "#00B1BD";
-    Error[0].innerHTML = "Ok!";
-    password.style.border = "1px solid #00B1BD";
-    password.style.color = "#00B1BD";
-    Error[1].style.color = "#00B1BD";
-    Error[1].innerHTML = "Ok!";
-    log = true;
-  }
-  if (log == true) form.addEventListener("submit", logged(name.value));
-}
-
-//user logged in
-function logged(username) {
-  changePageAnimation(login, userDash, "next");
-}
+function showMenu() {}
